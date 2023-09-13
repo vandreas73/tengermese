6173db5b983242fd6b8990bf33eb006ad2ee3d02
vandreas73 <113762114+vandreas73@users.noreply.github.com>
2023-09-13 09:52:10 +0200

    SearchEverywhere plugin beginned

---
+++ src/Plugins/Nop.Plugin.Widgets.SearchEverywhere/Class1.cs
@@ -0,0 +1,49 @@
+﻿using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+
+public class HelloWorldPlugin : BasePlugin, IWidgetPlugin
+{
+    /// <summary>
+    /// Gets a value indicating whether to hide this plugin on the widget list page in the admin area
+    /// </summary>
+    public bool HideInWidgetList => false;
+
+    /// <summary>
+    /// Gets a type of a view component for displaying widget
+    /// </summary>
+    /// <param name="widgetZone">Name of the widget zone</param>
+    /// <returns>View component type</returns>
+    public Type GetWidgetViewComponent(string widgetZone)
+    {
+        return typeof(ExampleWidgetViewComponent);
+    }
+
+    /// <summary>
+    /// Gets widget zones where this widget should be rendered
+    /// </summary>
+    /// <returns>
+    /// A task that represents the asynchronous operation
+    /// The task result contains the widget zones
+    /// </returns>
+    public Task<IList<string>> GetWidgetZonesAsync()
+    {
+        return Task.FromResult<IList<string>>(new List<string> { PublicWidgetZones.HomepageBeforeCategories });
+    }
+
+    public override async Task InstallAsync()
+    {
+        //Logic during installation goes here...
+
+        await base.InstallAsync();
+    }
+
+    public override async Task UninstallAsync()
+    {
+        //Logic during uninstallation goes here...
+
+        await base.UninstallAsync();
+    }
+}
---
+++ src/Plugins/Nop.Plugin.Widgets.SearchEverywhere/Components/ExampleWidgetViewComponent.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Nop.Web.Framework.Components;
+
+public class ExampleWidgetViewComponent : NopViewComponent
+{
+    public IViewComponentResult Invoke(string widgetZone, object additionalData)
+    {
+        return Content("Hello World");
+    }
+}
